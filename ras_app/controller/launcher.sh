0a52b11ab5dd9eb47b3523c05b02442799800f91
Giulio Garbi <julius.grb@gmail.com>
2022-03-30 12:57:37 +0200

    ...

--- ras_app/controller/launcher.sh
+++ ras_app/controller/launcher.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-python3 makeMtxs.py /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat < ~/out.all.8.txt
-git add /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat
-git commit -m "New mat"
-git push
+#python3 makeMtxs.py /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat < ~/out.all.8.txt
+#git add /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat
+#git commit -m "New mat"
+#git push
 
-for i in 15 45 30 18 43 28 33 20 40 35 23 25 38
+for i in 45 30 18 43 28 33 20 40 35 23 25 38
 do 
 	JAVA_HOME=/usr/lib/java/jdk-15.0.2 python3 ts_imp_sys.py $i 0.01 4 > ~/out.$i.4.txt
 	{ cat ~/out.all.8.txt ; tail -n 30 `ls -d ~/out*.4.txt -tr` ; } | python3 makeMtxs.py /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat 
-	git add /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat
-	git commit -m "New mat"
-	git push
+	#git add /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat
+	#git commit -m "New mat"
+	#git push
 done 
 
 for i in 38 113 75 44 106 81 50 100 69 56 94 63 88
 do 
 	JAVA_HOME=/usr/lib/java/jdk-15.0.2 python3 ts_imp_sys.py $i 0.01 10 > ~/out.$i.10.txt
 	{ cat ~/out.all.8.txt ; tail -n 30 `ls -d ~/out*.4.txt -tr` ; tail -n 30 `ls -d ~/out*.10.txt -tr` ; } | python3 makeMtxs.py /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat 
-	git add /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat
-	git commit -m "New mat"
-	git push
+	#git add /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat
+	#git commit -m "New mat"
+	#git push
 done
