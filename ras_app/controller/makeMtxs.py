a20cf7a5b4a177a10be60007d4456b2df459db5f
Giulio Garbi <julius.grb@gmail.com>
2022-03-30 09:41:32 +0200

    ...

--- ras_app/controller/makeMtxs.py
+++ ras_app/controller/makeMtxs.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 	TmCI = []
 	NC = []
 	NT = []
-	entryNames = np.array(["Client", "persistence", "auth", "image", "webui"], dtype=object)
+	entryNames = np.array(["Client", "persistence", "webui"], dtype=object)
 	taskNames = np.array(["Client", "all"], dtype=object)
 	record = False
 	for line in sys.stdin:
@@ -23,10 +23,10 @@ if __name__ == '__main__':
 			record = True
 			parts = line.strip().split()
 			Cli.append([int(parts[1])])
-			RTm.append([0.0]*5)
-			RTmCI.append([[0.0, 0.0]]*5)
-			Tm.append([0.0]*5)
-			TmCI.append([[0.0, 0.0]]*5)
+			RTm.append([0.0]*3)
+			RTmCI.append([[0.0, 0.0]]*3)
+			Tm.append([0.0]*3)
+			TmCI.append([[0.0, 0.0]]*3)
 			NC.append([float('inf'), int(parts[2])])
 			NT.append([float('inf'), float('inf')])
 		elif line.startswith("-----"):
@@ -40,15 +40,15 @@ if __name__ == '__main__':
 				elif parts[1].startswith("persistence"):
 					RTm[-1][1] = float(parts[2])
 					RTmCI[-1][1] = [float(parts[3][1:-1]), float(parts[4][:-1])]
-				elif parts[1].startswith("auth"):
+					#elif parts[1].startswith("auth"):
+					#	RTm[-1][2] = float(parts[2])
+					#	RTmCI[-1][2] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+					#elif parts[1].startswith("image"):
+					#	RTm[-1][3] = float(parts[2])
+					#	RTmCI[-1][3] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+				elif parts[1].startswith("webui"):
 					RTm[-1][2] = float(parts[2])
 					RTmCI[-1][2] = [float(parts[3][1:-1]), float(parts[4][:-1])]
-				elif parts[1].startswith("image"):
-					RTm[-1][3] = float(parts[2])
-					RTmCI[-1][3] = [float(parts[3][1:-1]), float(parts[4][:-1])]
-				elif parts[1].startswith("webui"):
-					RTm[-1][4] = float(parts[2])
-					RTmCI[-1][4] = [float(parts[3][1:-1]), float(parts[4][:-1])]
 			elif parts[0] == 'thr':
 				if parts[1].startswith("Client"):
 					Tm[-1][0] = float(parts[2])
@@ -56,15 +56,15 @@ if __name__ == '__main__':
 				elif parts[1].startswith("persistence"):
 					Tm[-1][1] = float(parts[2])
 					TmCI[-1][1] = [float(parts[3][1:-1]), float(parts[4][:-1])]
-				elif parts[1].startswith("auth"):
+					#elif parts[1].startswith("auth"):
+					#	Tm[-1][2] = float(parts[2])
+					#	TmCI[-1][2] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+					#elif parts[1].startswith("image"):
+					#	Tm[-1][3] = float(parts[2])
+					#	TmCI[-1][3] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+				elif parts[1].startswith("webui"):
 					Tm[-1][2] = float(parts[2])
 					TmCI[-1][2] = [float(parts[3][1:-1]), float(parts[4][:-1])]
-				elif parts[1].startswith("image"):
-					Tm[-1][3] = float(parts[2])
-					TmCI[-1][3] = [float(parts[3][1:-1]), float(parts[4][:-1])]
-				elif parts[1].startswith("webui"):
-					Tm[-1][4] = float(parts[2])
-					TmCI[-1][4] = [float(parts[3][1:-1]), float(parts[4][:-1])]
 	savemat(outfn,{'Cli':np.array(Cli), 'RTm':np.array(RTm), 'RTmCI':np.array(RTmCI), 'Tm':np.array(Tm), \
 		'TmCI':np.array(TmCI), 'NC':np.array(NC), 'NT':np.array(NT), 'entryNames':entryNames, \
 		'taskNames':taskNames})
