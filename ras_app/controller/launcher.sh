98d831a266895cdb85bae05f8cbe58d2c6379134
Giulio Garbi <julius.grb@gmail.com>
2022-04-03 11:21:30 +0200

    ...

--- ras_app/controller/launcher.sh
+++ ras_app/controller/launcher.sh
@@ -5,20 +5,26 @@
 #git commit -m "New mat"
 #git push
 
-for i in 45 30 18 43 28 33 20 40 35 23 25 38
+for i in 493 153 296 478 156 280 447 159 340 426 182 255 389
 do 
-	JAVA_HOME=/usr/lib/java/jdk-15.0.2 python3 ts_imp_sys.py $i 0.01 4 > ~/out.$i.4.txt
-	{ cat ~/out.all.8.txt ; tail -n 30 `ls -d ~/out*.4.txt -tr` ; } | python3 makeMtxs.py /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat 
-	#git add /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat
-	#git commit -m "New mat"
-	#git push
+	JAVA_HOME=/usr/lib/java/jdk-15.0.2 python3 ts_imp_sys.py $i 0.01 8 > ~/out.$i.8.txt
+	python3 makeMtxs.py /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat < ~/out.$i.8.txt
 done 
 
-for i in 38 113 75 44 106 81 50 100 69 56 94 63 88
+for i in 570 153 343 523 227 312 466 278 443 463 297 306 450
 do 
 	JAVA_HOME=/usr/lib/java/jdk-15.0.2 python3 ts_imp_sys.py $i 0.01 10 > ~/out.$i.10.txt
-	{ cat ~/out.all.8.txt ; tail -n 30 `ls -d ~/out*.4.txt -tr` ; tail -n 30 `ls -d ~/out*.10.txt -tr` ; } | python3 makeMtxs.py /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat 
-	#git add /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat
-	#git commit -m "New mat"
-	#git push
+	python3 makeMtxs.py /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat < ~/out.$i.10.txt
+done
+
+for i in 124 31 67 122 32 66 113 43 83 108 48 56 106
+do 
+	JAVA_HOME=/usr/lib/java/jdk-15.0.2 python3 ts_imp_sys.py $i 0.01 2 > ~/out.$i.2.txt
+	python3 makeMtxs.py /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat < ~/out.$i.2.txt
+done
+
+for i in 240 67 170 210 100 147 198 102 176 192 119 138 188
+do 
+	JAVA_HOME=/usr/lib/java/jdk-15.0.2 python3 ts_imp_sys.py $i 0.01 4 > ~/out.$i.4.txt
+	python3 makeMtxs.py /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat /home/giulio/fitMS-scripts/ras_app/ras_teastore_server/three_tier.mat < ~/out.$i.4.txt
 done
---
+++ ras_app/controller/makeMtxsMerge.py
@@ -0,0 +1,85 @@
+import sys
+import numpy as np
+import scipy
+from scipy.io import savemat, loadmat
+
+def mtx2str(name, data):
+	return name+" = ["+";".join(" ".join(str(x) for x in ln) for ln in data)+"];"
+
+if __name__ == '__main__':
+	outfn = sys.argv[1]
+	infn = sys.argv[2]
+	Cli = []
+	RTm = []
+	RTmCI = []
+	Tm = []
+	TmCI = []
+	NC = []
+	NT = []
+	entryNames = np.array(["Client", "persistence", "webui"], dtype=object)
+	taskNames = np.array(["Client", "all"], dtype=object)
+	# load old data
+	loaded = dict()
+	loadmat(infn, loaded)
+	nCases = loaded['Cli'].shape[0]
+	for i in range(nCases):
+		Cli.append(loaded['Cli'][i])
+		RTm.append(loaded['RTm'][i])
+		RTmCI.append(loaded['RTmCI'][i])
+		Tm.append(loaded['Tm'][i])
+		TmCI.append(loaded['TmCI'][i])
+		NC.append(loaded['NC'][i])
+		NT.append(loaded['NT'][i])
+	# read fresh data from stdin
+	record = False
+	for line in sys.stdin:
+		if line.startswith("True"):
+			record = True
+			parts = line.strip().split()
+			Cli.append([int(parts[1])])
+			RTm.append([0.0]*3)
+			RTmCI.append([[0.0, 0.0]]*3)
+			Tm.append([0.0]*3)
+			TmCI.append([[0.0, 0.0]]*3)
+			NC.append([float('inf'), int(parts[2])])
+			NT.append([float('inf'), float('inf')])
+		elif line.startswith("-----"):
+			record = False
+		elif record:
+			parts = line.strip().split()
+			if len(parts) > 0:
+				if parts[0] == 'rt':
+					if parts[1].startswith("Client"):
+						RTm[-1][0] = float(parts[2])
+						RTmCI[-1][0] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+					elif parts[1].startswith("persistence"):
+						RTm[-1][1] = float(parts[2])
+						RTmCI[-1][1] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+						#elif parts[1].startswith("auth"):
+						#	RTm[-1][2] = float(parts[2])
+						#	RTmCI[-1][2] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+						#elif parts[1].startswith("image"):
+						#	RTm[-1][3] = float(parts[2])
+						#	RTmCI[-1][3] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+					elif parts[1].startswith("webui"):
+						RTm[-1][2] = float(parts[2])
+						RTmCI[-1][2] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+				elif parts[0] == 'thr':
+					if parts[1].startswith("Client"):
+						Tm[-1][0] = float(parts[2])
+						TmCI[-1][0] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+					elif parts[1].startswith("persistence"):
+						Tm[-1][1] = float(parts[2])
+						TmCI[-1][1] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+						#elif parts[1].startswith("auth"):
+						#	Tm[-1][2] = float(parts[2])
+						#	TmCI[-1][2] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+						#elif parts[1].startswith("image"):
+						#	Tm[-1][3] = float(parts[2])
+						#	TmCI[-1][3] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+					elif parts[1].startswith("webui"):
+						Tm[-1][2] = float(parts[2])
+						TmCI[-1][2] = [float(parts[3][1:-1]), float(parts[4][:-1])]
+	savemat(outfn,{'Cli':np.array(Cli), 'RTm':np.array(RTm), 'RTmCI':np.array(RTmCI), 'Tm':np.array(Tm), \
+		'TmCI':np.array(TmCI), 'NC':np.array(NC), 'NT':np.array(NT), 'entryNames':entryNames, \
+		'taskNames':taskNames})
